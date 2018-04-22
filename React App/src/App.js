a8b0fb7f878449e7c68bdf7ed24bf94febad6a07
Jozi <jozigila@cit.edu.al>
2018-04-22 17:16:11 +0200

    Coins are instantiated as contracts and the name is displayed in a list

--- React App/src/App.js
+++ React App/src/App.js
@@ -13,8 +13,6 @@ class App extends Component {
       coins: [],
       activeAddress: null
     }
-
-    this.getCreatedCoins = this.getCreatedCoins.bind(this);
   }
 
   componentWillMount() {
@@ -39,7 +37,6 @@ class App extends Component {
 
       coin_factory.deployed().then((instance) => {
         this.setState({factoryInstance : instance});
-        // this.createTestCoin();
       });
 
       console.log(this.state.accounts);
@@ -47,27 +44,18 @@ class App extends Component {
     });
   }
 
-  getCreatedCoins(address) {
-    this.state.factoryInstance.creationBlock.call().then((results) => {
-      var factoryEvent = this.state.factoryInstance.CoinCreated({owner: address}, {fromBlock: results.valueOf() - 1, toBlock: 50});
-
-      factoryEvent.get((error, results) => {
-        if (error)
-          console.log('Error in event handler: ' + error);
-        else
-          console.log(results);
-      });
-    });
-  }
-
   render() {
-    let containerStyle = {
-      margin: 50
-    }
-
     return (
-      <div style={containerStyle}> 
-        <NewCoinForm factoryInstance={this.state.factoryInstance}/>
+      <div className="container">
+        <div className="row"> 
+          <div className="col-md-4 col-md-offset-4">
+            <NewCoinForm factoryInstance={this.state.factoryInstance} activeAddress={this.state.activeAddress} />
+            <br />
+            { this.state.factoryInstance ? 
+            <CoinDisplay web3={this.state.web3} factoryInstance={this.state.factoryInstance} activeAddress={this.state.activeAddress} /> : null
+            }
+          </div>
+        </div>
       </div>
     );
   }
@@ -90,43 +78,40 @@ class NewCoinForm extends React.Component {
   }
 
   render() {
-    let formStyle = {
-      position: 'relative',
-      float: 'left',
-      border: "1px solid black",
-      padding: 15
-    }
-
-    let tableRowStyle = {
-      margin: 30
+    let rowStyle = {
+      margin: 5,
     }
 
     return (
-        <form style={formStyle} onSubmit={this.handleSubmit}> 
-          <h2> Create Coin </h2>
-          <table><tbody>
-            <tr style={tableRowStyle}>
-              <td>Name:</td>
-              <td><input type="text" name="name" value={this.state.name} onChange={this.handleChange}/></td>
-            </tr>
-            <tr style={tableRowStyle}>
-              <td>Symbol:</td>
-              <td><input type="text" name="symbol"value={this.state.symbol} onChange={this.handleChange} /></td>
-            </tr>
-            <tr style={tableRowStyle}>
-              <td>Decimals:</td>
-              <td><input type="number" name="decimals" value={this.state.decimals} onChange={this.handleChange}/></td>
-            </tr>
-            <tr style={tableRowStyle}>
-              <td>Supply:</td>
-              <td><input type="number" name="supply" value={this.state.supply} onChange={this.handleChange}/></td>
-            </tr>
-            <tr style={tableRowStyle}>
-              <td></td>
-              <td><input type="submit" value="Create Coin"/></td>
-            </tr>
-          </tbody></table>
+      <div className="row" style={{border: '1px solid black', padding: 10, marginTop: 30}}>
+        <form onSubmit={this.handleSubmit}> 
+          <h3><strong> Create Coin </strong></h3>
+          <br/>
+          <div className="row" style={rowStyle}>
+              <div className="col-md-5">Account:</div>
+              <div className="col-md-7"><span><b>{String(this.props.activeAddress).slice(0, 6) + '....' + String(this.props.activeAddress).slice(-15) }</b></span></div>
+          </div>
+          <div className="row" style={rowStyle}>
+              <div className="col-md-5">Name:</div>
+              <div className="col-md-7"><input style={{width: '100%'}} type="text" name="name" value={this.state.name} onChange={this.handleChange}/></div>
+          </div>
+          <div className="row" style={rowStyle}>
+              <div className="col-md-5">Symbol:</div>
+              <div className="col-md-7"><input style={{width: '100%'}} type="text" name="symbol" value={this.state.symbol} onChange={this.handleChange}/></div>
+          </div>
+          <div className="row" style={rowStyle}>
+              <div className="col-md-5">Decimals:</div>
+              <div className="col-md-7"><input style={{width: '100%'}} type="number" name="decimals" value={this.state.decimals} onChange={this.handleChange}/></div>
+          </div>
+          <div className="row" style={rowStyle}>
+              <div className="col-md-5">Supply:</div>
+              <div className="col-md-7"><input style={{width: '100%'}} type="number" name="supply" value={this.state.supply} onChange={this.handleChange}/></div>
+          </div>
+          <div className="row" style={rowStyle}>
+              <div className="col-md-3"><input type="submit" value="Create Coin"/></div>
+          </div>
         </form>
+      </div>
     );
   }
 
@@ -142,17 +127,101 @@ class NewCoinForm extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    if (!this.state.name || !this.state.symbol || !this.state.decimals || !this.state.supply) return;
-    alert("Coin being created")
+    if (!this.state.name || !this.state.symbol || !this.state.supply) return;
+
     this.createCoin(this.state.name, this.state.symbol, this.state.decimals, this.state.supply);
     event.stopPropagation();
   }
 
   createCoin(name, symbol, decimals, supply) {
-    this.props.factoryInstance.createCoin(name, symbol, decimals, supply, {from: this.state.activeAddress}).then((result) => 
+    this.props.factoryInstance.createCoin(name, symbol, decimals, supply, {from: this.props.activeAddress}).then((result) => 
     {
       console.log("Coin created: " + name);
       console.log(result);
+    });
+  }
+}
+
+class CoinDisplay extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      coinInstances: []
+    }
+
+    this.getCreatedCoins = this.getCreatedCoins.bind(this);
+    this.getCreatedCoins(this.props.activeAddress, this.props.factoryInstance)
+  }
+
+  render(){
+    let displayStyle = {
+      border: "1px solid black"
+    }
+
+    return(
+      <div className="row" style={displayStyle}>
+        {this.state.coinInstances.map((coinInstance, key) => <SingleCoin instance={coinInstance} activeAddress={this.props.activeAddress} key={key}/>)}
+      </div>
+    );
+  }
+
+  getCreatedCoins(address, factoryInstance) {
+    factoryInstance.creationBlock.call().then((results) => {
+      var factoryEvent = factoryInstance.CoinCreated({owner: address}, {fromBlock: results.valueOf() - 1, toBlock: 'latest'});
+
+      const contract = require('truffle-contract');
+      const coin_contract = contract(Coin);
+      coin_contract.setProvider(this.props.web3.currentProvider);
+
+      factoryEvent.watch((error, results) => {
+        coin_contract.at(results.args.contractAddress).then((instance) => {
+          console.log(instance)
+          if (this.state.coinInstances.filter((element) => {return element.address === instance.address}).length < 1){
+            this.setState(prevState => ({
+              coinInstances: [...prevState.coinInstances, instance]
+            }));
+          }
+        });
+      });
+    });
+  }
+}
+
+class SingleCoin extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: null,
+      symbol: null,
+      decimals: null,
+      balances: []
+    }
+
+    this.getCoinInfo = this.getCoinInfo.bind(this);
+    this.getCoinInfo();
+  }
+
+  render(){
+    return(
+      <h1> {this.state.name} </h1>
+    );
+  }
+
+  getCoinInfo(){
+    this.props.instance.name.call().then((result) => {
+      this.setState({name: result});
+    });
+
+    this.props.instance.symbol.call().then((result) => {
+      this.setState({symbol: result});
+    });
+
+    this.props.instance.decimals.call().then((result) => {
+      this.setState({decimals: result});
+    });
+
+    this.props.instance.balanceOf.call(this.props.activeAddress).then((result) => {
+      
     });
   }
 }
